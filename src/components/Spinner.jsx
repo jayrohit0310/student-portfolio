73e2749b32d90f9154352c5ad9ee466af3121652
jayrohit0310 <jayrohit0310@gmail.com>
2026-07-23 10:43:42 +0530

    Add Spinner component for loading state

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,1 +1,18 @@
 
+function Spinner() {
+  return (
+    <div className="spinner-container">
+
+      <div className="spinner"></div>
+
+      <h2>Loading Repositories...</h2>
+
+      <p>
+        Please wait while we fetch the latest GitHub repositories.
+      </p>
+
+    </div>
+  );
+}
+
+export default Spinner;
